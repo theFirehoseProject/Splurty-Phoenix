ca930561e31a2426f1d0484a46d60226f90310ca
Ken Mazaika <ken@thefirehoseproject.com>
2015-01-24 17:37:49 -0500

    Lesson 5 - creating a quote in the database

--- web/controllers/quote_controller.ex
+++ web/controllers/quote_controller.ex
@@ -1,6 +1,9 @@
 defmodule Splurty.QuoteController do
   use Phoenix.Controller
 
+  alias Splurty.Router
+  import Splurty.Router.Helpers
+
   plug :action
 
   def homepage(conn, _params) do
@@ -11,6 +14,17 @@ defmodule Splurty.QuoteController do
     conn
     |> assign(:quotes, Repo.all(Splurty.Quote))
     |> render("index.html")
+  end
+
+  def new(conn, _params) do
+    render conn, "new.html"
+  end
+
+  def create(conn, %{"quote" => %{"saying" => saying, "author" => author}}) do
+    q = %Splurty.Quote{saying: saying,  author: author}
+    Repo.insert(q)
+
+    redirect conn, to: quote_path(conn, :index)
   end
 
 end
--- web/view.ex
+++ web/view.ex
@@ -13,5 +13,9 @@ defmodule Splurty.View do
     end
   end
 
+  def csrf_token(conn) do
+    Plug.Conn.get_session(conn, :csrf_token)
+  end
+  
   # Functions defined here are available to all other views/templates
 end
